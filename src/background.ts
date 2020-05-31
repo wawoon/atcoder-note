de4936830fca33ca982d2aaf485bb8ac2c647e9f
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-05-31 10:07:29 +0900

    feature: load extention on atcoder.jp

--- src/background.ts
+++ src/background.ts
@@ -13,7 +13,7 @@ chrome.runtime.onInstalled.addListener(function () {
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: "developer.chrome.com" },
+            pageUrl: { hostEquals: "atcoder.jp" },
           }),
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()],
